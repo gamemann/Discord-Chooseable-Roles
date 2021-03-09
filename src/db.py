98cb2d7f1f4414580ef5e782f878ce0883c7ce36
gamemann <gamemann@gflclan.com>
2021-03-09 17:56:55 +0000

    Add helper function for optionally updating columns.

--- src/db.py
+++ src/db.py
@@ -6,3 +6,24 @@ def connect():
 
     return conn
 
+def updateoptional(conn, table, setparams, whereparams):
+    cur = conn.cursor()
+
+    toset = []
+    towhere = []
+    values = []
+
+    for key in setparams:
+        toset.append(key + "=?")
+        values.append(setparams[key])
+
+    for key in whereparams:
+        towhere.append(key + "=?")
+        values.append(whereparams[key])
+
+    setstr = ", ".join(toset)
+    wherestr = "AND ".join(towhere)
+
+    querystr = "UPDATE " + table + " SET " + setstr + " WHERE " + wherestr
+
+    cur.execute(querystr, values)
