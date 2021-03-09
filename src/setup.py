e2f986778c64008522d64b63f76be1645908b8a2
gamemann <gamemann@gflclan.com>
2021-03-09 02:17:07 +0000

    Remove servers table and add reactions table.

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,8 @@ import sqlite3
 def setuptables(conn):
     cur = conn.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS `servers` (id integer PRIMARY KEY, name text)")
+    #cur.execute("CREATE TABLE IF NOT EXISTS `servers` (id integer PRIMARY KEY, name text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS `reactions` (id integer PRIMARY KEY AUTOINCREMENT, userid integer, msgid integer, reaction text)")
     cur.execute("CREATE TABLE IF NOT EXISTS `messages` (id integer PRIMARY KEY, guildid integer)")
     cur.execute("CREATE TABLE IF NOT EXISTS `config` (key text PRIMARY KEY, value text)")
     cur.execute("CREATE TABLE IF NOT EXISTS `permissions` (guildid integer PRIMARY KEY, roles text)")
